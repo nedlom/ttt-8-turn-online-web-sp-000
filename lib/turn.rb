b2675e46c90402ad4ee19abe26e1dd962b8e3cf8
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-09 18:08:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,7 @@ def move(board, index, player = "X")
 end
 
 def turn(board)
-  puts "Plearse enter 1-9:"
+  puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
