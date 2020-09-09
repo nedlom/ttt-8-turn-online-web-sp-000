7b3dce78ed1ebb90b3af069541c98c87e83b1332
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-09 18:00:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,9 +19,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && !position_taken?(board, index)
-    true
-  end
+  index.between?(0, 8) && !position_taken?(board, index)
 end
 
 def move(board, index, player = "X")
